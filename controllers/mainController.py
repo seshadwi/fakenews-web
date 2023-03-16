1b5ce66dec7e80b22cb406bc3d86b34ac61ab400
seshadwi <seshadwi@gmail.com>
2023-03-16 13:44:44 +0700

    UPDATE 1. menambah halaman home 2. menambah halaman prediksi

--- controllers/mainController.py
+++ controllers/mainController.py
@@ -2,3 +2,6 @@ from flask import render_template, redirect, url_for
 
 def index():
     return render_template('main/index.html')
+
+def prediction():
+    return render_template('main/predict.html')
--- routes/web.py
+++ routes/web.py
@@ -4,3 +4,4 @@ from controllers import mainController
 blueprint = Blueprint('web', __name__)
 
 blueprint.route('/', methods=['GET'])(mainController.index)
+blueprint.route('/predict', methods=['GET'])(mainController.prediction)
