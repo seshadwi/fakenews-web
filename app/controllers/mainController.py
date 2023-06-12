f13ae1816dafbe7ec8c35f6db74b1c36508c4ea4
seshadwi <seshadwi@gmail.com>
2023-06-12 13:37:14 +0700

    UPDATE

--- app/controllers/mainController.py
+++ app/controllers/mainController.py
@@ -9,17 +9,41 @@ def prediction():
     return render_template('main/predict.html')
 
 def details(id):
-    res = Results.query.filter_by(id=id).first().to_dict()
-    resultjs = json.loads(res['result'])
-    result = {
-        'matchWUP': json.loads(resultjs['result']['datamatchWUP']),
-        'scoreWUP' :  resultjs['result']['WUP'],
-        'matchPATH': json.loads(resultjs['result']['datamatchPATH']),
-        'scorePATH' :  resultjs['result']['PATH']
-    }
-    return render_template(
-        'main/results.html', 
-        resultScrab=json.loads(resultjs['scrapdata']),
-        processData=json.loads(resultjs['process']),
-        result = result
-    )
+    res = Results.query.filter_by(id=id).first()
+    if res:
+        res = res.to_dict()
+        resultjs = json.loads(res['result'])
+        result = {
+            'matchWUP': json.loads(resultjs['result']['datamatchWUP']),
+            'scoreWUP' :  resultjs['result']['WUP'],
+            'matchPATH': json.loads(resultjs['result']['datamatchPATH']),
+            'scorePATH' :  resultjs['result']['PATH']
+        }
+        return render_template(
+            'main/results.html', 
+            id=id,
+            resultScrab=json.loads(resultjs['scrapdata']),
+            processData=json.loads(resultjs['process']),
+            result = result
+        )
+    return redirect(url_for('web.prediction'))
+
+def detailSrap(id):
+    res = Results.query.filter_by(id=id).first()
+    if res:
+        res = res.to_dict()
+        resultjs = json.loads(res['result'])
+        result = {
+            'matchWUP': json.loads(resultjs['result']['datamatchWUP']),
+            'scoreWUP' :  resultjs['result']['WUP'],
+            'matchPATH': json.loads(resultjs['result']['datamatchPATH']),
+            'scorePATH' :  resultjs['result']['PATH']
+        }
+        return render_template(
+            'main/resultdetail.html', 
+            id=id,
+            resultScrab=json.loads(resultjs['scrapdata']),
+            processData=json.loads(resultjs['process']),
+            result = result
+        )
+    return redirect(url_for('web.prediction'))
